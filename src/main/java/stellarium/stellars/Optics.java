6d12b0cc2bdf11dc209836bb8641eafa964d9b60
PitchBright <johnnytweed@hotmail.com>
2016-01-04 21:58:13 -0500

    Working Turbulence

--- src/main/java/stellarium/stellars/Optics.java
+++ src/main/java/stellarium/stellars/Optics.java
@@ -10,13 +10,13 @@ import stellarium.config.IConfigHandler;
 public class Optics implements IConfigHandler {
 	
 	// MagOffset needs to be changed to whatever the maximum Magnitude of Venus is 
-	private static final float magOffset = 5.0f;
-	private static final float magCompressionBase = 6.5f;
+	private static final float magOffset = 5.25f;
+	private static final float magCompressionBase = 6.50f;
 	
 	private Property brightnessContrastProperty;
 	
 	//BrightnessContrast needs to be added to the user config and set to StellarSky.getManager().brightnesscontrast
-	private double brightnessContrast = 1.5;
+	private double brightnessContrast = 2.0;
 	private float magCompression;
 	private float magContrast;
 	
@@ -26,11 +26,11 @@ public class Optics implements IConfigHandler {
 	
 	@Override
 	public void setupConfig(Configuration config, String category) {
-		brightnessContrastProperty = config.get(category, "Brightness_Contrast", 1.75);
-		brightnessContrastProperty.comment = "Brightness Contrast determines the contrast"
-				+ "between bright stars and faint stars."
-				+ "The bigger the value, the less difference between bright stars and faint stars."
-				+ "For real world, 1.0 is right. Though default value is 1.75 for visual effect.";
+		brightnessContrastProperty = config.get(category, "Brightness_Contrast", 2.0);
+		brightnessContrastProperty.comment = "Brightness Contrast determines the contrast "
+				+ "between bright stars and faint stars. "
+				+ "The bigger the value, the less difference between bright stars and faint stars. "
+				+ "Real world (minimum) = 1.0. Default = 2.0 for visual effect.";
 		brightnessContrastProperty.setRequiresMcRestart(false);
 		brightnessContrastProperty.setLanguageKey("config.property.client.brcontrast");
 	}
@@ -43,20 +43,19 @@ public class Optics implements IConfigHandler {
 	}
 	
 	public static float getAlphaFromMagnitudeSparkling(float Mag, float bglight){
-		double turb = StellarSky.getManager().turb * randomTurbulance.nextGaussian();
-		return getAlpha(Mag * instance.magCompression + turb * instance.brightnessContrast, bglight);
+		double turb = randomTurbulance.nextGaussian() * ((StellarSky.getManager().turb) / (Mag + 4.46f));
+		return getAlpha(((Mag + 1.46f) * instance.magCompression) + turb, bglight);
 	}
 
 	public static float getAlphaFromMagnitude(double Mag, float bglight) {
 		return getAlpha(Mag * instance.magCompression, bglight);
+//		return getAlpha(0, bglight);
 	}
 	
 	public static final double constantBgDiv = Math.log(2.1333334f + 1.0f);
 	
 	private static float getAlpha(double magCompressed, float bglight) {
-		return (float) ((Math.pow(instance.magContrast,
-				- magCompressed - magOffset * ((Math.pow(Math.log(bglight + 1.0f)/constantBgDiv, magOffset)))))
-				- (bglight / 2.1333334f));
+		return (float) ((Math.pow(instance.magContrast,	- magCompressed - magOffset * ((Math.pow(Math.log(bglight + 1.0f)/constantBgDiv, magOffset)))))	- (bglight / 2.1333334f));
 	}
 	
 }
