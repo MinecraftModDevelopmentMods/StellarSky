4e5bf4e8ec367992156d5ab5dc9c27e40067f94a
PitchBright <johnnytweed@hotmail.com>
2016-02-28 16:36:02 -0500

    pr5 fix

--- src/main/java/stellarium/stellars/Optics.java
+++ src/main/java/stellarium/stellars/Optics.java
@@ -16,26 +16,6 @@ public class Optics implements IConfigHandler {
 	
 	private Property brightnessContrastProperty;
 	
-<<<<<<< HEAD
-	public static float getAlphaFromMagnitude(float Mag, float bglight){
-
-		BrightnessContrast = 1.75f;
-		//BrightnessContrast needs to be added to the user config and set to StellarSky.getManager().brightnesscontrast
-		MagOffset = 5.0f;
-		// MagOffset needs to be changed to whatever the maximum Magnitude of Venus is 
-		MagLimit = StellarSky.getManager().mag_Limit;
-		MagCompression = (float) 6.5f/MagLimit;
-		MagContrast = (float) ((float) ((Math.pow(2.512, (1/(BrightnessContrast * MagCompression))))));
-
-		Random randomno = new Random();
-		float NewTurb = StellarSky.getManager().turb * (5.0f * (float) randomno.nextFloat());
-
-		MagTurb = (float) ((NewTurb / (Mag + 3.46f)));	
-		MagCompressed = (float) ((Mag * MagCompression) + MagTurb);
-		
-		return (float) ((Math.pow(MagContrast, MagCompressed * -1.0f)) / (Math.pow(MagContrast, MagOffset * ((Math.pow(Math.log(bglight + 1.0f), MagOffset) / (Math.pow(Math.log(2.1333334f + 1.0f), MagOffset)))))) - (bglight / 2.1333334f)) ;
-		
-=======
 	//BrightnessContrast needs to be added to the user config and set to StellarSky.getManager().brightnesscontrast
 	private double brightnessContrast = 2.0;
 	private float magCompression;
@@ -55,7 +35,6 @@ public class Optics implements IConfigHandler {
 				+ "Real world (minimum) = 1.0. Default = 2.0 for visual effect.";
 		brightnessContrastProperty.setRequiresMcRestart(false);
 		brightnessContrastProperty.setLanguageKey("config.property.client.brcontrast");
->>>>>>> pr/5
 		
 		settings = StellarSky.proxy.getClientSettings();
 	}
@@ -72,17 +51,6 @@ public class Optics implements IConfigHandler {
 		return getAlpha(((Mag + 1.46f) * instance.magCompression) + turb, bglight);
 	}
 
-<<<<<<< HEAD
-		BrightnessContrast = 1.75f;
-		MagOffset = 5.0f;
-		MagLimit = StellarSky.getManager().mag_Limit;
-		MagCompression = (float) 6.5f/MagLimit;
-		MagCompressed = (float) ((Mag * MagCompression));
-		MagContrast = (float) ((float) ((Math.pow(2.512, (1/(BrightnessContrast * MagCompression))))));
-				
-		return (float) ((Math.pow(MagContrast, MagCompressed * -1.0f)) / (Math.pow(MagContrast, MagOffset * ((Math.pow(Math.log(bglight + 1.0f), MagOffset) / (Math.pow(Math.log(2.1333334f + 1.0f), MagOffset)))))) - (bglight / 2.1333334f)) ;
-		
-=======
 	public static float getAlphaFromMagnitude(double Mag, float bglight) {
 		return getAlpha(Mag, bglight);
 	}
@@ -91,7 +59,6 @@ public class Optics implements IConfigHandler {
 	
 	private static float getAlpha(double magCompressed, float bglight) {
 		return (float) ((Math.pow(instance.magContrast,	- magCompressed - magOffset * ((Math.pow(Math.log(bglight + 1.0f)/constantBgDiv, magOffset)))))	- (bglight / 2.1333334f));
->>>>>>> pr/5
 	}
 	
 }
