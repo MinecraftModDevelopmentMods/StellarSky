8d03d2847c41ee0d5ae3302c2a17234e19949115
Abastro <abab9579@gmail.com>
2016-03-28 22:24:18 +0900

    Some more
    (This would be an error in github client)

---
+++ src/main/java/stellarium/stellars/system/MoonRenderCache.java
@@ -0,0 +1,70 @@
+package stellarium.stellars.system;
+
+import sciapi.api.value.IValRef;
+import sciapi.api.value.euclidian.EVector;
+import stellarium.client.ClientSettings;
+import stellarium.stellars.Optics;
+import stellarium.stellars.sketch.IRenderCache;
+import stellarium.stellars.util.ExtinctionRefraction;
+import stellarium.stellars.view.IStellarViewpoint;
+import stellarium.util.math.SpCoord;
+import stellarium.util.math.Spmath;
+import stellarium.util.math.VecMath;
+
+public class MoonRenderCache implements IRenderCache<Moon> {
+	
+	protected SpCoord appCoord, cache;
+	protected int latn, longn;
+	protected EVector moonPos[][], moonnormal[][];
+	protected float moonilum[][];
+	protected EVector buf = new EVector(3);
+	protected double size, difactor, appMag;
+
+	@Override
+	public void initialize(ClientSettings settings) {
+		this.appCoord = new SpCoord();
+		this.latn = settings.imgFrac;
+		this.longn = 2*settings.imgFrac;
+		this.moonPos = new EVector[longn][latn+1];
+		this.moonilum = new float[longn][latn+1];
+		this.moonnormal = new EVector[longn][latn+1];
+		this.cache = new SpCoord();
+	}
+
+	@Override
+	public void updateCache(ClientSettings settings, Moon object, IStellarViewpoint viewpoint) {
+		EVector ref = new EVector(3);
+		ref.set(viewpoint.getProjection().transform(object.earthPos));
+		double airmass = viewpoint.getAirmass(ref, false);
+		double mag = object.currentMag + airmass * Optics.ext_coeff_V;
+		appCoord.setWithVec(ref);
+		viewpoint.applyAtmRefraction(this.appCoord);
+		
+		this.size = object.radius/Spmath.getD(VecMath.size(object.earthPos));
+		this.difactor = 0.8 / 180.0 * Math.PI / this.size;
+		this.difactor = this.difactor * this.difactor / Math.PI;
+		
+		this.size *= (98.0*5.0);
+		
+		int latc, longc;
+		for(longc=0; longc<longn; longc++){
+			for(latc=0; latc<=latn; latc++){
+				buf.set(object.posLocalM((double)longc/(double)longn*360.0, (double)latc/(double)latn*180.0-90.0));
+				moonilum[longc][latc]=(float) (object.illumination(buf) * this.difactor * 1.5);
+				moonnormal[longc][latc] = new EVector(3).set(buf);
+				buf.set(object.posLocalG(buf));
+				buf.set(VecMath.mult(50000.0, buf));
+				IValRef ref2 = viewpoint.getProjection().transform(buf);
+				
+				cache.setWithVec(ref2);
+				viewpoint.applyAtmRefraction(this.cache);
+
+				moonPos[longc][latc] = cache.getVec();
+
+				if(cache.y < 0)
+					moonilum[longc][latc]=0.0f;
+			}
+		}
+	}
+
+}
--- src/main/java/stellarium/stellars/system/Planet.java
+++ src/main/java/stellarium/stellars/system/Planet.java
@@ -1,15 +1,9 @@
 package stellarium.stellars.system;
 
-import java.util.ArrayList;
-
-import sciapi.api.value.IValRef;
 import sciapi.api.value.euclidian.EVector;
-import stellarium.stellars.Optics;
-import stellarium.stellars.OldSatellite;
-import stellarium.stellars.util.ExtinctionRefraction;
+import stellarium.stellars.sketch.IRenderCache;
 import stellarium.util.math.Rotate;
 import stellarium.util.math.Spmath;
-import stellarium.util.math.VecMath;
 
 public class Planet extends SolarObject {
 	
@@ -17,24 +11,20 @@ public class Planet extends SolarObject {
 	protected double a0, e0, I0, L0, wbar0, Omega0;
 	protected double ad, ed, Id, Ld, wbard, Omegad;
 	protected double b, c, s, f;
-	
-	//Satellites
-	ArrayList<Satellite> satellites=new ArrayList();
 
-	Rotate roti = new Rotate('X'), rotw = new Rotate('Z'), rotom = new Rotate('Z');
+	private Rotate roti = new Rotate('X'), rotw = new Rotate('Z'), rotom = new Rotate('Z');
 	
-
-	public Planet(boolean isRemote) {
-		super(isRemote);
+	public Planet(boolean isRemote, SolarObject parent) {
+		super(isRemote, parent);
 	}
 	
-	public void addSatellite(Satellite sat){
-		sat.parPlanet=this;
-		satellites.add(sat);
+	@Override
+	public IRenderCache generateCache() {
+		return new PlanetRenderCache();
 	}
 
 	@Override
-	public EVector getEcRPos(double year) {
+	public EVector getRelativePos(double year) {
 		double cen=year/100.0;
 		double a=a0+ad*cen,
 				e=e0+ed*cen,
@@ -50,6 +40,11 @@ public class Planet extends SolarObject {
 		rotom.setRAngle(-Spmath.Radians(Omega));
 		
 		return Spmath.GetOrbVec(a, e, roti, rotw, rotom, M);
+	}
+
+	@Override
+	public int getRenderId() {
+		return LayerSolarSystem.planetRenderId;
 	}
 
 }
