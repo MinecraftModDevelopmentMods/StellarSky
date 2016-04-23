0e78d1560f61b1b9f35543a4464e7f599f35ccd2
Abastro <abab9579@gmail.com>
2016-04-23 23:08:10 +0900

    Stellar API Version Update.
    Release version of Stellar Sky v0.1.3.5

--- src/main/java/stellarium/StellarSky.java
+++ src/main/java/stellarium/StellarSky.java
@@ -24,7 +24,7 @@ import stellarium.world.provider.DefaultWorldProviderReplacer;
 import stellarium.world.provider.EndReplacer;
 
 @Mod(modid=StellarSky.modid, version=StellarSky.version,
-	dependencies="required-after:StellarAPI@[0.1.1.4c, 0.1.2.0)", guiFactory="stellarium.config.StellarConfigGuiFactory")
+	dependencies="required-after:StellarAPI@[0.1.1.4d, 0.1.2.0)", guiFactory="stellarium.config.StellarConfigGuiFactory")
 public class StellarSky {
 	
 		public static final String modid = "stellarsky";
